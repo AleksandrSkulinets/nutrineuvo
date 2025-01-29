5a34cb09bd6a263fa96867c2d667627a93d6bd9c
AleksandrSkulinets <129200582+AleksandrSkulinets@users.noreply.github.com>
2025-01-29 21:00:42 +0200

    tailwindcss
    Installed tailwind css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="w-full h-[50px] flex items-center justify-between">
+       <h1 className="ml-10 bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-5xl font-extrabold text-transparent">Logo</h1>
+      <ul className="flex space-x-4 mr-10">
+        <li>link1</li>
+        <li>link2</li>
+        <li>link3</li>
+      </ul>
       </header>
-    </div>
+    </>
   );
 }
 
