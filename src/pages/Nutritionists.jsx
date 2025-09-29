7f1a93f70ef9dd55adc87f49d5899af79a7e1c8d
Arthur Ferreira <arthur.ferreiramiran@gmail.com>
2025-09-29 15:58:59 +0300

    updates

--- src/pages/Nutritionists.jsx
+++ src/pages/Nutritionists.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { Button } from "../components/ui/button";
 import { Link } from "react-router-dom";
-import { ArrowRight, Rocket, Globe, BookOpen, Users } from "lucide-react";
+import {  Rocket, Globe, BookOpen, Users } from "lucide-react";
 import { useTranslation } from "react-i18next";
 
 const Nutritionists = () => {
