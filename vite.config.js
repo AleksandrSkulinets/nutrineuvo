772bd2ffce0bff0cd6cabdd3cfa845f8dd4a8153
Arthur Ferreira <arthur.ferreiramiran@gmail.com>
2025-10-21 18:27:28 +0300

    vite config changed

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,24 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 5174, 
-  },
-  base: "/homepage/",
-  build: {
-    outDir: "build", 
-    emptyOutDir: true,
-  },
+export default defineConfig(({ mode }) => {
+  const basePath =
+    process.env.PUBLIC_URL || // ✅ use env var if provided (from CI/CD)
+    (mode === "development"
+      ? "/" // local dev
+      : "/wp-content/reactpress/apps/nutrineuvo/build/"); // default production path
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: 5174,
+    },
+    base: basePath,
+    build: {
+      outDir: "build",
+      emptyOutDir: true,
+    },
+  };
 });
