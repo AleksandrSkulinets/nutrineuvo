504b6b0488b1b5cde08c9dbc8bada5c980e7f048
Arthur Ferreira <arthur.ferreiramiran@gmail.com>
2025-10-21 19:42:20 +0300

    testing..

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 5174, 
   },
-  base: "/wp-content/reactpress/apps/nutrineuvo/build/",
+  base: "/",
   build: {
     outDir: "build", 
     emptyOutDir: true,
